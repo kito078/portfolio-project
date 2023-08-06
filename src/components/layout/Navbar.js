a5e5120491d317b3136102bd392b5a7b60940213
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-06 20:30:10 +0200

    dd

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,41 +1,9 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// function Navbar() {
-//   return (
-//     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
-//       <div className="container mx-auto">
-//         <div className="flex-none px-2 mx-2">
-//           {/* <FaGithub className="inline pr-2 text-3xl" /> */}
-//           <Link to="/" className="text-lg font-bold align-middle">
-//             GithubUser
-//           </Link>
-//         </div>
-
-//         <div className="flex-1 px-2 mx-2">
-//           <div className="flex justify-end">
-//             <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
-//               Home
-//             </Link>
-//             <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
-//               About
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
-import SearchMeal from "../Meals/SearchMeal";
+// import { Link } from "react-router-dom";
+
 import { DiGrails } from "react-icons/di";
 import { FaGithub } from "react-icons/fa";
-import Dropback from "./Dropback";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -43,40 +11,30 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
   return (
-    <nav className="mb-12 shadow-lg bg-neutral relative mx-auto p-2 px-6">
+    <nav className="mb-12  bg-neutral relative mx-auto p-2 px-6">
       <div className="container-one">
         {/* flex container for Nav Items */}
         <div className="flex items-center justify-between space-x-20 my-2">
           {/* logo */}
 
-          <div className="flex-none  hidden lg:block px-2 mx-2">
+          <div className="flex  px-2 mx-2">
             <DiGrails className="inline pr-2 text-4xl" />
-            <Link to="/" className="text-lg font-bold align-middle">
-              MealDay
-            </Link>
+            <h4 className="text-lg font-bold align-middle">MealDay</h4>
           </div>
-          {/* Search */}
-          <SearchMeal />
+
           {/* Menu Items */}
           <div className="hidden items-center space-x-10 uppercase text-grayisBlue md:flex">
-            <Link
-              to="/about"
-              className="tracking-widest hover:text-softRed text-1xl tex-center"
-            >
-              <Dropback />
-            </Link>
-            <Link
+            {/* <Link
               to="/"
               className="tracking-widest hover:text-softRed text-1xl"
-            >
-              Home |
-            </Link>
+            > */}
+            Home |
+            {/* </Link>
             <Link
               to="/about"
               className="tracking-widest hover:text-softRed text-1xl"
-            >
-              About |
-            </Link>
+            > */}
+            About |{/* </Link> */}
           </div>
           {/* hamburger Button */}
           <button
@@ -108,9 +66,6 @@ function Navbar() {
             <a href="#about" className="block hover:text-softRed">
               About
             </a>
-          </div>
-          <div className="w-full py-3 text-center">
-            <Dropback />
           </div>
         </div>
       </div>
