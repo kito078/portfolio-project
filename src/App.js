de64b82de76fb9db14217c3c5eac13812edf4640
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-06 21:35:54 +0200

    s

--- src/App.js
+++ src/App.js
@@ -2,43 +2,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-
-// function DarkThemeProvider() {
-// const [theme, setTheme] = useState("dark");
-
-// useEffect(() => {
-//   if (theme === "dark") {
-//     document.documentElement.classList.add("dark");
-//   } else {
-//     document.documentElement.classList.remove("dark");
-//   }
-// }, [theme]);
-
-// const toggleTheme = () => {
-//   if (theme === "dark") {
-//     setTheme("light");
-//     localStorage.setItem("theme", "light"); // Optional: Save user preference in local storage
-//   } else {
-//     setTheme("dark");
-//     localStorage.setItem("theme", "dark"); // Optional: Save user preference in local storage
-//   }
-// };
-
-//   return (
-//     <div>
-//       {/* Your app content here */}
-//       <button onClick={toggleTheme}>
-//         Switch to {theme === "dark" ? "Light" : "Dark"} Theme
-//       </button>
-//     </div>
-//   );
-// }
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/shwcase/Shocase.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Shocase() {
+  return (
+    <div>
+      <h1>Brittany Chiang</h1>
+      <h5>Lead Engineer at Upstatement</h5>
+      <p>
+        I build accessible, inclusive products and digital experiences for the
+        web.
+      </p>
+      <span>about</span>
+    </div>
+  );
+}
+
+export default Shocase;
