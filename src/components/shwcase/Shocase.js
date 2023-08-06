eab7efb8ba8cec6c51cf07a12b864b2e51ca9248
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-06 22:14:26 +0200

    s

--- src/components/shwcase/Shocase.js
+++ src/components/shwcase/Shocase.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Shocase() {
   return (
-    <div className="mx-8">
-      <h1 className="text-4xl  sm:text-5xl font-bold text-gray-300 mb-3">
-        Brittany Chiang
+    <div className="mx-8 lg:mx-0 w-96 ">
+      <h1 className="text-4xl leading-normal sm:text-5xl font-bold text-gray-300 mb-3">
+        Ngqabutho Kito Ndlovu
       </h1>
       <h5 className="text-2xl font-semibold text-gray-300 mb-3">
-        Lead Engineer at Upstatement
+        Front-End Developer
       </h5>
       <p className="text-gray-400 text-lg">
         I build accessible, inclusive products and digital experiences for the
