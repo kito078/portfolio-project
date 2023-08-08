ee121ae60763a9b822b826f78ad4e492cdda5074
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-09 01:19:24 +0200

    ee

--- src/components/buildSection/BuildSection.js
+++ src/components/buildSection/BuildSection.js
@@ -3,14 +3,13 @@ import Build from "../../images/build.png";
 
 function BuildSection() {
   return (
-    <div>
-      {" "}
-      <div class="h-96 flex items-center justify-center ">
-        <div class="max-w-md w-full p-6 bg-white rounded-lg shadow-md overflow-y-scroll">
-          <div class="mb-4">
-            <img src={Build} alt="Your Image" class="w-full h-auto" />
+    <div className="relative h-64 overflow-y-scroll scrollbar-none hover:bg-gray-800">
+      <div className="absolute flex items-center justify-center">
+        <div className="w-96 bg-white rounded-lg shadow-md">
+          <div className="mb-4">
+            <img src={Build} alt="Your Image" className="w-full h-auto" />
           </div>
-          <p class="text-gray-700 text-sm">
+          <p className="text-gray-700 text-sm">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
             est ut urna cursus accumsan.
           </p>
