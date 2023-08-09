6d9f5bbe189e8aa8dfe21466bbb72e01e883c791
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-10 01:00:44 +0200

    <div className="h-64 relative overflow-hidden">   <div className="flex items-center justify-center">     <div className="w-96 bg-white rounded-lg shadow-md overflow-hidden">       <div className="relative">         <img src={Build} alt="Your Image" className="w-full h-auto" />         <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300"></div>       </div>       <p className="text-gray-700 text-sm">         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at         est ut urna cursus accumsan.       </p>     </div>   </div> </div>

--- src/components/buildSection/BuildSection.js
+++ src/components/buildSection/BuildSection.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Build from "../../images/build.png";
+import "./BuildSection.css"; // Import your CSS file
 
 function BuildSection() {
   return (
-    <div className="h-64 overflow-y-scroll scrollbar-none ">
-      <div className=" flex items-center justify-center">
-        <div className="w-96 bg-white rounded-lg shadow-md">
+    <div className="main-container h-64 overflow-y-scroll scrollbar-none relative">
+      <div className="flex items-center justify-center">
+        <div className="w-full bg-white rounded-lg shadow-md relative">
+          <div className="overlay">
+            {/* This is the overlay content */}
+            <h2 className="text-white text-2xl">Overlay Content</h2>
+          </div>
           <div className="mb-4">
             <img src={Build} alt="Your Image" className="w-full h-auto" />
           </div>
@@ -20,3 +25,17 @@ function BuildSection() {
 }
 
 export default BuildSection;
+
+//  <div className="h-64 overflow-y-scroll scrollbar-none ">
+//       <div className=" flex items-center justify-center">
+//         <div className="w-96 bg-white rounded-lg shadow-md">
+//           <div className="mb-4">
+//             <img src={Build} alt="Your Image" className="w-full h-auto" />
+//           </div>
+//           <p className="text-gray-700 text-sm">
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at
+//             est ut urna cursus accumsan.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
