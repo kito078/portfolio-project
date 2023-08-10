cc1a01a5216bcbb47ec19d46411bd8e64a2d7759
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-10 15:02:40 +0200

    s

--- src/components/contact/Touch.js
+++ src/components/contact/Touch.js
@@ -4,7 +4,7 @@ function Touch() {
   return (
     <div className="h-96">
       <p>Feel free to contact me anytimes</p>
-      <h3 className="text-white">Get in Touch</h3>
+      <h3 className="text-white text-5xl font-bold">Get in Touch</h3>
       <hr className="border-t-2 border-gray-600 my-2 w-20" />
     </div>
   );
