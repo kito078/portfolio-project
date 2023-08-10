0c2522fb288a0e49b4caa4aaf73774816533b5bb
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-10 11:16:38 +0200

    ss

--- src/components/follow/Follow.js
+++ src/components/follow/Follow.js
@@ -3,8 +3,16 @@ import React from "react";
 function Follow() {
   return (
     <div>
-      <h5 className="text-white">follow me</h5>
-      <div class="h-12 bg-gray-300 w-1 text-white"></div>
+      <div class="writing-mode-vertical">
+        <h5 class="text-white transform rotate-180 origin-center whitespace-nowrap">
+          follow me
+        </h5>
+      </div>
+
+      <h5 className="text-white transform -rotate-90">follow me</h5>
+      <div class="flex">
+        <div class="border-l-2 border-gray-600 h-10 my-2"></div>
+      </div>
     </div>
   );
 }
