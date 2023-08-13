e51bbf91c7a6e177a4bb543859b94c03fbd2d380
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-13 02:16:07 +0200

    c

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div className="flex flex-col items-center justify-content-center h-48 mt-16 text-center mx-6 lg:mx-0">
       <p className="text-gray-400 text-lg mb-4">Get to know me</p>
-      <h3 className="text-gray-300 text-3xl md:text-5xl font-bold mb-7">
+      <h3 className="text-gray-300 text-4xl md:text-6xl font-bold mb-7">
         About Me
       </h3>
       <hr className="border-t-4 border-blue-500  my-2 w-20" />
