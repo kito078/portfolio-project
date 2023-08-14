59a7ef75f1a24bc7b26d131faa6293add4b83fa9
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-14 13:18:27 +0200

    s

--- src/components/skills/SkillSection.js
+++ src/components/skills/SkillSection.js
@@ -3,23 +3,20 @@ import Html from "../../images/j.png";
 
 function SkillSection() {
   return (
-    return (
-        <div className="grid grid-rows-3 gap-4 items-center justify-center">
-          <div className="flex flex-col items-center">
-            <img className="w-20 mb-4" src={Html} alt="" />
-            <h5 className="uppercase font-bold text-gray-300">html</h5>
-          </div>
-          <div className="flex flex-col items-center">
-            <img className="w-20 mb-4" src={Html} alt="" />
-            <h5 className="uppercase font-bold text-gray-300">html</h5>
-          </div>
-          <div className="flex flex-col items-center">
-            <img className="w-20 mb-4" src={Html} alt="" />
-            <h5 className="uppercase font-bold text-gray-300">html</h5>
-          </div>
-        </div>
-      );
-      
+    <div className="grid grid-rows-3 gap-4 items-center justify-center">
+      <div className="flex flex-col items-center">
+        <img className="w-20 mb-4" src={Html} alt="" />
+        <h5 className="uppercase font-bold text-gray-300">html</h5>
+      </div>
+      <div className="flex flex-col items-center">
+        <img className="w-20 mb-4" src={Html} alt="" />
+        <h5 className="uppercase font-bold text-gray-300">html</h5>
+      </div>
+      <div className="flex flex-col items-center">
+        <img className="w-20 mb-4" src={Html} alt="" />
+        <h5 className="uppercase font-bold text-gray-300">html</h5>
+      </div>
+    </div>
   );
 }
 
