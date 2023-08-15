70b31dd401787d6a92565e67006beeb682c0470a
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-15 23:15:09 +0200

    ss

--- src/components/skills/SkillSection.js
+++ src/components/skills/SkillSection.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Box from "../../images/j.png";
+import Box2 from "../../images/c.png";
 
 function SkillSection() {
   return (
-    <div className="grid-one col-span-2">
-      <div>heed</div>
-      <div>heed</div>
-      <div>heed</div>
-      <div>heed</div>
+    <div class="grid grid-cols-3 gap-4">
+      <div class="bg-blue-200 p-4">
+        <img src={Box} alt="" />
+      </div>
+      <div class="bg-blue-200 p-4">
+        <img src={Box2} alt="" />
+      </div>
+      <div class="bg-green-200 p-4">Div 2</div>
+      <div class="bg-yellow-200 p-4">Div 3</div>
     </div>
   );
 }
