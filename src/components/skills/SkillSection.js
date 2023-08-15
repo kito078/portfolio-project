c56109c1017da36386e70ba91fda73231a665812
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-15 12:47:27 +0200

    f

--- src/components/skills/SkillSection.js
+++ src/components/skills/SkillSection.js
@@ -3,7 +3,11 @@ import Html from "../../images/j.png";
 
 function SkillSection() {
   return (
-    <div class="grid grid-rows-2 hover:grid-rows-6">
+    <div class="flex hover:grid-rows-6">
+      <div className="flex flex-col items-center">
+        <img className="w-20 mb-4" src={Html} alt="" />
+        <h5 className="uppercase font-bold text-gray-300">html</h5>
+      </div>
       <div className="flex flex-col items-center">
         <img className="w-20 mb-4" src={Html} alt="" />
         <h5 className="uppercase font-bold text-gray-300">html</h5>
