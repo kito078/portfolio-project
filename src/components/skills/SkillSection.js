485cf9bc237a19ecca8c49aeb87469655580ed7a
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-17 08:25:40 +0200

    d

--- src/components/skills/SkillSection.js
+++ src/components/skills/SkillSection.js
@@ -1,6 +1,11 @@
 import React from "react";
-import Box from "../../images/j.png";
+import Box from "../../images/h.png";
 import Box2 from "../../images/c.png";
+import Box3 from "../../images/b.png";
+import Box4 from "../../images/t.png";
+import Box5 from "../../images/j.png";
+import Box6 from "../../images/g.png";
+import Box7 from "../../images/r.png";
 
 function SkillSection() {
   return (
@@ -13,31 +18,37 @@ function SkillSection() {
       </div>
       <div class="flex flex-col items-center p-4 ">
         <div>
-          <img className="w-20" src={Box} alt="" />
+          <img className="w-20" src={Box2} alt="" />
         </div>
         <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
       </div>
       <div class="flex flex-col items-center p-4 ">
         <div>
-          <img className="w-20" src={Box} alt="" />
+          <img className="w-20" src={Box3} alt="" />
         </div>
         <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
       </div>
       <div class="flex flex-col items-center p-4 ">
         <div>
-          <img className="w-20" src={Box} alt="" />
+          <img className="w-20" src={Box4} alt="" />
         </div>
         <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
       </div>
       <div class="flex flex-col items-center p-4 ">
         <div>
-          <img className="w-20" src={Box} alt="" />
+          <img className="w-20" src={Box5} alt="" />
         </div>
         <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
       </div>
       <div class="flex flex-col items-center p-4 ">
         <div>
-          <img className="w-20" src={Box} alt="" />
+          <img className="w-20" src={Box6} alt="" />
+        </div>
+        <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
+      </div>
+      <div class="flex flex-col items-center p-4 ">
+        <div>
+          <img className="w-20" src={Box7} alt="" />
         </div>
         <h5 className="font-semibold text-md mt-4 uppercase">Html</h5>
       </div>
